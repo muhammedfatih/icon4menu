1210462314d54368cbe32007b9f7de2739c79d66
muhammedfatih <mfatihinanc@gmail.com>
2019-03-06 09:07:45 +0300

    External sources are fetched from local.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Icon4Menu
  * Plugin URI: https://github.com/muhammedfatih/icon4menu
  * Description: Helps the usage of country flag icons on menus
- * Version: 0.0.1
+ * Version: 0.2
  * Author: Muhammed Fatih
  * Author URI: https://www.mfatih.com
  */
@@ -12,7 +12,7 @@ add_filter('wp_nav_menu_objects', 'insertIconToMenuItemOnPages', 10, 2);
 function insertIconToMenuItemOnPages( $items, $args ) {
 	foreach( $items as &$item ) {
 		if( preg_match("#\[(.*?)\](.*)#", "{$item->title}", $titleParts) && count($titleParts)>2) {
-			$item->title = '<i class="flag-icon flag-icon-'.$titleParts[1].'"></i> '.$titleParts[2];
+			$item->title = '<i class="flag-icon flag-icon-'.strtolower($titleParts[1]).'"></i> '.$titleParts[2];
 		}
     }
 	return $items;	
@@ -20,6 +20,7 @@ function insertIconToMenuItemOnPages( $items, $args ) {
 
 add_action('wp_enqueue_scripts', 'insertIcon2Menu');
 function insertIcon2Menu() {
-    wp_register_style('namespace', 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.css');
-    wp_enqueue_style('namespace');
+	$plugin_url = plugin_dir_url( __FILE__ );
+	wp_enqueue_style('flag-icon.min.css', $plugin_url . '/css/flag-icon.min.css');
+    wp_enqueue_script('flag-icon.js', $plugin_url. '/js/docs.js');
 }
